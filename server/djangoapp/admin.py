10097730502f3a9494768c0fc0c00afa194343ef
Peter Yamout <peter.yamout2001@gmail.com>
2026-04-05 11:31:33 -0400

    added car models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,5 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
 
@@ -10,4 +7,5 @@
 
 # CarMakeAdmin class with CarModelInline
 
-# Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
